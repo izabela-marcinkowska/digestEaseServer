33949f9fe4dd45658dde442c39b6fd42d87980bb
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-12-08 18:08:55 +0100

    feat: Handle adding new repport

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 import express, { Request, Response } from "express";
 import cors from "cors";
-
+import "dotenv/config";
+import { format, startOfMonth, endOfMonth } from "date-fns";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors());
+
+import OpenAI from "openai";
+const openai = new OpenAI();
 
 const supabase = createClient(
   "https://jtvdkdhziojophcpguoh.supabase.co",
@@ -58,6 +62,68 @@ app.post("/add-log", async (req: Request, res: Response) => {
   }
 });
 
+async function getRecordsForCurrentMonth() {
+  // Get the first and last day of the current month
+  const firstDayOfMonth = format(startOfMonth(new Date()), "yyyy-MM-dd");
+  const lastDayOfMonth = format(endOfMonth(new Date()), "yyyy-MM-dd");
+
+  try {
+    let { data, error } = await supabase
+      .from("Logs")
+      .select("*")
+      .gte("date", firstDayOfMonth)
+      .lte("date", lastDayOfMonth);
+
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+app.get("/openai", async (req, res) => {
+  const dataFromThisMonth = await getRecordsForCurrentMonth();
+  console.log(dataFromThisMonth);
+  const currentDate = format(new Date(), "yyyy-MM-dd");
+  const dataForChat = JSON.stringify(dataFromThisMonth);
+  try {
+    let data = "";
+    const stream = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Analyze IBS patient data to identify connections between dietary patterns, stress levels, alcohol consumption, and the severity and frequency of bowel movements and pain. Emphasize how alcohol, stress, and certain foods might influence IBS symptoms. Address the analysis directly to 'you,' providing a summarized view of your dietary habits over the past month and their correlation with your symptoms. Deliver your findings in a succinct manner, with no more than five to seven sentences, focusing on five primary insights or trends derived from the data. Refrain from suggesting general lifestyle changes or dietary advice, and avoid discussing the variability of IBS symptoms among individuals or the necessity for medical consultation. The objective is to present a targeted analysis of how your diet and stress levels are related to your IBS symptoms, based on the data provided, while maintaining a direct and personalized approach. ${dataForChat}`,
+        },
+      ],
+      stream: true,
+    });
+    for await (const chunk of stream) {
+      data += chunk.choices[0]?.delta?.content || "";
+    }
+    console.log(data);
+
+    const { error: rapportError } = await supabase.from("Rapports").insert([
+      {
+        date: currentDate,
+        result: data,
+      },
+    ]);
+
+    if (rapportError) {
+      throw rapportError;
+    }
+    res.json({ data });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    } else {
+      res.status(500).send("An unknown error occurred");
+    }
+  }
+});
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
